1f8fc33beff9002e756ae316d53ae97ce41eb689
PaukIsUha <mihakosh@gmail.com>
2025-07-01 15:42:41 +0300

    Fix new v

--- PartnerBot/notifier.py
+++ PartnerBot/notifier.py
@@ -2,8 +2,13 @@ from configs import NOTIFIER_CONFIGS
 import requests
 import httpx
 
+NOTIFIES_IS_ACTIVE = False
+
 
 async def register_send(name, phone, email, field_info):
+    if not NOTIFIES_IS_ACTIVE:
+        return
+
     payload = {
         "data": {
             "name": name,
@@ -13,24 +18,36 @@ async def register_send(name, phone, email, field_info):
         }
     }
 
-    async with httpx.AsyncClient() as client:
-        r = await client.post(NOTIFIER_CONFIGS.register_url(), json=payload)
-    print(r.status_code, r.text)
+    try:
+        async with httpx.AsyncClient() as client:
+            r = await client.post(NOTIFIER_CONFIGS.register_url(), json=payload)
+        print(r.status_code, r.text)
+    except ValueError:
+        print("Нет подключения к Notifier Service")
 
 
 async def reg_send(username):
+    if not NOTIFIES_IS_ACTIVE:
+        return
+
     payload = {
         "data": {
             "username": username,
         }
     }
 
-    async with httpx.AsyncClient() as client:
-        r = await client.post(NOTIFIER_CONFIGS.reg_url(), json=payload)
-    print(r.status_code, r.text)
+    try:
+        async with httpx.AsyncClient() as client:
+            r = await client.post(NOTIFIER_CONFIGS.reg_url(), json=payload)
+        print(r.status_code, r.text)
+    except ValueError:
+        print("Нет подключения к Notifier Service")
 
 
 async def contact_send(username, name, phone, email, field_info):
+    if not NOTIFIES_IS_ACTIVE:
+        return
+
     payload = {
         "data": {
             "username": username,
@@ -47,6 +64,9 @@ async def contact_send(username, name, phone, email, field_info):
 
 
 async def products_send(name, phone, email, field_info):
+    if not NOTIFIES_IS_ACTIVE:
+        return
+
     payload = {
         "data": {
             "name": name,
@@ -62,6 +82,9 @@ async def products_send(name, phone, email, field_info):
 
 
 async def start_edu_send(username, name, phone, email, field_info):
+    if not NOTIFIES_IS_ACTIVE:
+        return
+
     payload = {
         "data": {
             "username": username,
@@ -78,6 +101,9 @@ async def start_edu_send(username, name, phone, email, field_info):
 
 
 async def finish_edu_send(username, name, phone, email, field_info):
+    if not NOTIFIES_IS_ACTIVE:
+        return
+
     payload = {
         "data": {
             "username": username,
@@ -94,6 +120,9 @@ async def finish_edu_send(username, name, phone, email, field_info):
 
 
 async def get_indiv_send(username, name, phone, email, field_info):
+    if not NOTIFIES_IS_ACTIVE:
+        return
+
     payload = {
         "data": {
             "username": username,
